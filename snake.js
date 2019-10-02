d69c7b34489043538af9e6ba279e0e32178ddbea
Artur <tondamus@gmail.com>
2019-10-03 00:26:35 +0200

    Snake move
    Now the snake is moving.

--- snake.js
+++ snake.js
@@ -49,6 +49,20 @@ function draw() {
     }
     ctx.drawImage(foodImg, food.x, food.y);
 
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
+
+    if( d == "LEFT") snakeX -= box;
+    if( d == "UP") snakeY -= box;
+    if( d == "RIGHT") snakeX += box;
+    if( d == "DOWN") snakeY += box;
+    snake.pop(); 
+    let newHead = {
+        x : snakeX,
+        y : snakeY
+    }
+    snake.unshift(newHead);
+
     ctx.fillStyle = "white";
     ctx.font = "45px Changa one";
     ctx.fillText(score,box,box);
