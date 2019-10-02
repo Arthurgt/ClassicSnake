9da720dfb28f462222ac6e39558a9845bd4a405a
Artur <tondamus@gmail.com>
2019-10-03 00:03:38 +0200

    Added random generated food and snake head
    Added function do draw food in random position and head in the middle of the gameboard.

--- snake.js
+++ snake.js
@@ -4,8 +4,8 @@ const ctx = cvs.getContext("2d");
 const box = 32;
 const ground = new Image();
 ground.src = "img/ground.png";
-const food = new Image();
-food.src = "img/food.png";
+const foodImg = new Image();
+foodImg.src = "img/food.png";
 
 let snake = [];
 snake[0] = {
@@ -20,3 +20,18 @@ let food = {
 }
 
 let score = 0;
+
+function draw() {
+    ctx.drawImage(ground,0,0);
+
+    for( let i = 0; i<snake.length; i++){
+        ctx.fillStyle = (i == 0)? "green" : "white";
+        ctx.fillRect(snake[i].x, snake[i].y, box, box);
+
+        ctx.strokeStyle = "red";
+        ctx.strokeRect(snake[i].x, snake[i].y, box, box);
+    }
+    ctx.drawImage(foodImg, food.x, food.y);
+}
+
+let game = setInterval(draw,100);
